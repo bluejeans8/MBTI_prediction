676ce43196d8889a4c332ee5e07b3bec48c11180
bluejeans98 <jsk0821@kaist.ac.kr>
2022-05-31 21:17:25 +0900

    implement algorithm

--- algorithm.py
+++ algorithm.py
@@ -2,4 +2,16 @@ from konlpy.tag import Kkma
 from konlpy.utils import pprint
 
 kkma = Kkma()
-pprint(kkma.pos(u'오늘 머 먹었어?'))
+try:
+    f = open('kakao.txt', 'r', encoding='utf-8')
+    s = f.readlines()
+    for line in s:
+        try:
+            pos= kkma.pos(line)
+            print(pos)
+        except:
+            continue
+except:
+    print("error!")
+finally:
+    f.close()
