c283ab0c185e3930e6fffb98c7ce11ac0be1c4e0
bluejeans98 <jsk0821@kaist.ac.kr>
2022-06-01 23:23:43 +0900

    added prints

--- mbti_categories.py
+++ mbti_categories.py
@@ -10,4 +10,4 @@ class Mbti(IntEnum):
 
 
 # Morphological units
-mbti = [[('에', 'Josa')], [], [], []]
+mbti = [[('랑', 'Josa'), ('이랑', 'Josa'), ('자', 'Josa')], [('면', 'Josa'), ('텐데', 'Eomi')], [('우와', 'Exclamation'), ('아이구', 'Exclamation'), ('우아', 'Exclamation'), ('에구', 'Exclamation'), ('그랬구나', 'Exclamation'), ('정말', 'Exclamation')], [('내일', 'Noun'), ('일정', 'Noun'), ('약속', 'Noun'), ('계획', 'Noun')]]
